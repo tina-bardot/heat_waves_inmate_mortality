ca8131742e4394e408cba3516f9b2f5c15fdde00
tina-bardot <142334011+tina-bardot@users.noreply.github.com>
2023-12-05 11:06:39 -0500

    Update combine_prism_tx_DIC_max.R

--- code/texas/combine_prism_tx_DIC_max.R
+++ code/texas/combine_prism_tx_DIC_max.R
@@ -32,23 +32,31 @@ getwd()
 ## Subset PRISM data to cities in TX with DIC
 ################################################################################
 
-# Load data
+# Load data: get all cities that include deaths OR have facilities
 
 tx_dic <- readRDS("Data/Intermediate/TX/tx_DIC_clean.rds") %>%
   distinct(city) 
 
+# Jails
+
 coj_tx <- readRDS("Data/Intermediate/TX/coj_tx_city.rds") %>%
   distinct(city) 
+
+# State and federal prisons
 
 cocf_tx <- readRDS("Data/Intermediate/TX/cocf_tx_city.rds") %>%
   distinct(city) 
 
+# Combine to get a full city dataset
+
 tx_fac <- full_join(tx_dic, coj_tx) %>%
   full_join(cocf_tx)
 
+# Read PRISM temperature data
+
 prism <- read_feather('Data/Intermediate/Climate data/prism/maxt/tx_max_temp.feather') %>%
   mutate(city = gsub("THE COLONY", "TENNESSEE C", city)) %>%
-  filter(city %in% tx_fac$city)
+  filter(city %in% tx_fac$city) # filter to cities which exist in other datasets
 
 rm(cocf_tx, coj_tx, tx_dic)
 
@@ -58,29 +66,22 @@ tx_temps <- prism %>%
 saveRDS(tx_temps, "Data/Intermediate/TX/tx_fac_city_temps.rds")
 
 ################################################################################
-## Load tx DIC data
+## Load Texas Deaths in Custody data
 ################################################################################
 
 tx_dic <- readRDS("Data/Intermediate/TX/tx_DIC_clean.rds")
 
+# Change format to match PRISM date format
+
 tx_dic$death_ymd <- as.Date(tx_dic$death_ymd, format="%m/%d/%Y") 
 tx_dic$death_ymd <- format(tx_dic$death_ymd, format="%Y-%m-%d")
-
-
-################################################################################
-## Extract temp data from Tx temps 
-################################################################################
-
-
-tx_dic_temp <- full_join(tx_dic, tx_temps, by = join_by("city" == "city", "death_ymd" == "date"))
-
-tx_dic_nomatch <- anti_join(tx_dic, tx_temps, by = join_by("city" == "city", "death_ymd" == "date"))
-
-saveRDS(tx_dic_temp, "Data/Intermediate/TX/tx_DIC_max_t.rds")
 
 ################################################################################
 ## Count DIC per day around max temps
 ################################################################################
+
+# Left join to combine the DIC data to the temperature data, count number of deaths
+# per city per day
 
 tx_temp_dic <- left_join(tx_temps, tx_dic, by = join_by("date" == "death_ymd", "city" == "city"), relationship = "many-to-many") %>%
   group_by(date, city) %>%
@@ -90,5 +91,7 @@ tx_temp_dic <- left_join(tx_temps, tx_dic, by = join_by("date" == "death_ymd", "
    is.na(record_id) ~ 0,
    TRUE ~ death_count_total
   ))
+
+saveRDS(tx_temp_dic, "Data/Intermediate/TX/tx_temp_dic.rds")
 
 
